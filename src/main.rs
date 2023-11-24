0d7f89204e1f0a0ad8f6d25d645e51fa12bf9983
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2023-11-24 09:09:14 +0000

    Accept URL from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: spdrs <url>");
+        return;
+    }
+
+    let url = args
+        .get(1)
+        .expect("the index must exist due to previous len check");
+
+    println!("{url}");
 }
